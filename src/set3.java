a90558fcf2d44f3b59b1973d648a799ed0ec31d4
t00048641 <travis.m.slattery@students.ittralee.ie>
2019-09-12 15:18:09 +0100

    set3 update

--- src/set3.java
+++ src/set3.java
@@ -9,30 +9,31 @@ public class set3 {
         double lowRate = 0.20, highRate = 0.41;
         double taxFree = 20000;
         double lowTax = 36000;
+        double taxableIncome = totalIncome - taxFree;
 
         if (totalIncome <= 20000){
 
-            JOptionPane.showMessageDialog(null,"Hi " + name + "Your Tax rate is 0%.\nYour income is €" + String.format("%.2f",totalIncome),"Tax Details",
+            JOptionPane.showMessageDialog(null,"Hi " + name + " your Tax rate is 0%.\nYour income is €" + String.format("%.2f",totalIncome),"Tax Details",
                     JOptionPane.INFORMATION_MESSAGE);
         }
-        else if (totalIncome > 20000 || totalIncome <= 36000)
+        else if (totalIncome > 20000 && totalIncome <= 36000)
         {
             double lessTax = (totalIncome - taxFree) * lowRate;
-            double net = totalIncome - lessTax;
+            double net = taxableIncome - lessTax;
             double gross = taxFree + net;
 
-            JOptionPane.showMessageDialog(null,"Your Tax rate is 41%.\nYour income is €"  + String.format("%.2f",gross),"Tax Details",
+            JOptionPane.showMessageDialog(null,"Hi " + name + " your Tax rate is 20%.\nYour income is €"  + String.format("%.2f",gross),"Tax Details",
                     JOptionPane.INFORMATION_MESSAGE);
         }
         else
         {
             double stage1 = totalIncome - taxFree;
-            double stage2 = (lowTax - taxFree) * lowRate;
+            double stage2 = stage1 * lowRate;
             double stage3 = (totalIncome - lowTax) * highRate;
-            double net = stage1+stage2+stage3;
+            double net = taxableIncome - (stage2 + stage3);
             double gross = taxFree + net;
 
-            JOptionPane.showMessageDialog(null,"Your Tax rate is 20%.\nYour income is €"  + String.format("%.2f",gross),"Tax Details",
+            JOptionPane.showMessageDialog(null,"Hi " + name + " your Tax rate is 41%.\nYour income is €"  + String.format("%.2f",gross),"Tax Details",
                     JOptionPane.INFORMATION_MESSAGE);
         }
 
